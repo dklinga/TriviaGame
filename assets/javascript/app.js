110077bf8eb5d14477366b74db57410d49348574
Dan Klinga <dklinga@Dans-MacBook-Pro.local>
2018-03-05 14:37:06 -0500

    monday

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -20,6 +20,22 @@ $("#inlineRadio13").on("click", run);
 $("#inlineRadio14").on("click", run);
 $("#inlineRadio15").on("click", run);
 $("#inlineRadio16").on("click", run);
+$("#inlineRadio17").on("click", run);
+$("#inlineRadio18").on("click", run);
+$("#inlineRadio19").on("click", run);
+$("#inlineRadio20").on("click", run);
+$("#inlineRadio21").on("click", run);
+$("#inlineRadio22").on("click", run);
+$("#inlineRadio23").on("click", run);
+$("#inlineRadio24").on("click", run);
+$("#inlineRadio25").on("click", run);
+$("#inlineRadio26").on("click", run);
+$("#inlineRadio27").on("click", run);
+$("#inlineRadio28").on("click", run);
+$("#inlineRadio29").on("click", run);
+$("#inlineRadio30").on("click", run);
+$("#inlineRadio31").on("click", run);
+$("#inlineRadio32").on("click", run);
 
 //  Clearing the intervalId prior to setting our new intervalId will not allow multiple instances.
 function run() {
@@ -37,11 +53,13 @@ function decrement() {
         //  ...run the stop function.
         stop();
         //  Alert the user that time is up.
+        document.getElementById('roar').play();
         alert("Times Up! You got " + numberRight + " correct!!!");
         window.location.reload();
     }
-    if (numberAnswered === 4) {
+    if (numberAnswered === 8) {
         stop();
+        document.getElementById('roar').play();
         alert("Times Up! You got " + numberRight + " correct!!!");
         window.location.reload();
     }
@@ -144,6 +162,98 @@ function clickR() {
 
 function clickW() {
     if (document.getElementById("answer4").innerHTML = "Wrong!!!");
+    numberAnswered++;
+    console.log(numberAnswered);
+}
+
+//Q5
+
+document.getElementById("inlineRadio17").addEventListener("click", clickW5);
+document.getElementById("inlineRadio18").addEventListener("click", clickW5);
+document.getElementById("inlineRadio19").addEventListener("click", clickW5);
+document.getElementById("inlineRadio20").addEventListener("click", clickR5);
+
+function clickR5() {
+    if (document.getElementById("answer5").innerHTML = "You are correct!!!"); {
+        numberRight++;
+        numberAnswered++;
+        console.log(numberRight);
+        console.log(numberAnswered);
+    }
+
+}
+
+function clickW5() {
+    if (document.getElementById("answer5").innerHTML = "Wrong!!!");
+    numberAnswered++;
+    console.log(numberAnswered);
+}
+
+//Q6
+
+document.getElementById("inlineRadio21").addEventListener("click", clickW6);
+document.getElementById("inlineRadio22").addEventListener("click", clickR6);
+document.getElementById("inlineRadio23").addEventListener("click", clickW6);
+document.getElementById("inlineRadio24").addEventListener("click", clickW6);
+
+function clickR6() {
+    if (document.getElementById("answer6").innerHTML = "You are correct!!!"); {
+        numberRight++;
+        numberAnswered++;
+        console.log(numberRight);
+        console.log(numberAnswered);
+    }
+
+}
+
+function clickW6() {
+    if (document.getElementById("answer6").innerHTML = "Wrong!!!");
+    numberAnswered++;
+    console.log(numberAnswered);
+}
+
+//Q7
+
+document.getElementById("inlineRadio25").addEventListener("click", clickW7);
+document.getElementById("inlineRadio26").addEventListener("click", clickW7);
+document.getElementById("inlineRadio27").addEventListener("click", clickR7);
+document.getElementById("inlineRadio28").addEventListener("click", clickW7);
+
+function clickR7() {
+    if (document.getElementById("answer7").innerHTML = "You are correct!!!"); {
+        numberRight++;
+        numberAnswered++;
+        console.log(numberRight);
+        console.log(numberAnswered);
+    }
+
+}
+
+function clickW7() {
+    if (document.getElementById("answer7").innerHTML = "Wrong!!!");
+    numberAnswered++;
+    console.log(numberAnswered);
+}
+
+//Q8
+
+document.getElementById("inlineRadio29").addEventListener("click", clickW8);
+document.getElementById("inlineRadio30").addEventListener("click", clickR8);
+document.getElementById("inlineRadio31").addEventListener("click", clickW8);
+document.getElementById("inlineRadio32").addEventListener("click", clickW8);
+
+function clickR8() {
+    if (document.getElementById("answer8").innerHTML = "You are correct!!!"); {
+        numberRight++;
+        numberAnswered++;
+        console.log(numberRight);
+        console.log(numberAnswered);
+    }
+
+}
+
+function clickW8() {
+    if (document.getElementById("answer8").innerHTML = "Wrong!!!");
     numberAnswered++;
     console.log(numberAnswered);
 }
